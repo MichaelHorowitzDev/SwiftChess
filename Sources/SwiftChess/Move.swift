1eb5df0e95947cd215acaf88edcbd700d69db1bd
MichaelHorowitzDev <85199380+MichaelHorowitzDev@users.noreply.github.com>
2021-06-04 00:15:15 -0400

    Update Move.swift

--- Sources/SwiftChess/Move.swift
+++ Sources/SwiftChess/Move.swift
@@ -11,10 +11,15 @@ public struct Move: Equatable {
     public static func == (lhs: Move, rhs: Move) -> Bool {
         return lhs.from == rhs.from && lhs.to == rhs.to
     }
-    public let from: (x: Int, y: Int)
-    public let to: (x: Int, y: Int)
+    let from: (x: Int, y: Int)
+    let to: (x: Int, y: Int)
     let pieceMoved: Piece? = nil
     let captures: Bool = false
+    
+    public init(from: (x: Int, y: Int), to: (x: Int, y: Int)) {
+        self.from = from
+        self.to = to
+    }
     
 //     init(from: (x: Int, y: Int), to: (x: Int, y: Int), pieceMoved: Piece? = nil, captures: Bool = false) {
 //         self.from = (x: from.x-1, y: from.y-1)
