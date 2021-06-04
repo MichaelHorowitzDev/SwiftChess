170ea48ee9b766780a742566f782f307badc0593
MichaelHorowitzDev <85199380+MichaelHorowitzDev@users.noreply.github.com>
2021-06-04 00:38:54 -0400

    Update Piece.swift

--- Sources/SwiftChess/Piece.swift
+++ Sources/SwiftChess/Piece.swift
@@ -30,7 +30,7 @@ public enum MoveResult {
 }
 
 public protocol Piece {
-    var color: PieceColor { get }
-    func moves(x: Int, y: Int, board: [[Piece?]]) -> [Move]
-    func attackingSquares(x: Int, y: Int, board: [[Piece?]]) -> [(Int, Int)]
+    public var color: PieceColor { get }
+    public func moves(x: Int, y: Int, board: [[Piece?]]) -> [Move]
+    public func attackingSquares(x: Int, y: Int, board: [[Piece?]]) -> [(Int, Int)]
 }
